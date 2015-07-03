89867bd7c45edf3924a83a70e701725323a61857
vietleminh <leminhviet93@gmail.com>
2015-07-03 14:55:31 +0700

    init model

---
+++ db/migrate/20150702090148_create_results.rb
@@ -0,0 +1,12 @@
+class CreateResults < ActiveRecord::Migration
+  def change
+    create_table :results do |t|
+      t.references :lesson, index: true, foreign_key: true
+      t.integer :word_id
+      t.integer :answer_id
+
+      t.timestamps null: false
+    end
+    add_index :results, [:lesson_id, :created_id]
+  end
+end
