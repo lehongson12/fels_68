036ab2c815a32621e9537c3eca54d738d0653906
vietleminh <leminhviet93@gmail.com>
2015-07-06 09:38:35 +0700

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    @user.role = 0
+    if @user.save
+      flash[:success] = I18n.t "signup.signup_success"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+                                   :password_confirmation
+  end
+end
