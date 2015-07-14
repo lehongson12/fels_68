4997b5821fa7fdfc0c8bad7e02ca854506cd00cb
vietleminh <leminhviet93@gmail.com>
2015-07-14 11:21:49 +0700

    user_profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
+  end
+  
+  def edit
+    @user = current_user
   end
 
   def show
@@ -43,4 +48,13 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find params[:id]
   end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "login.require"
+      redirect_to login_url
+    end
+  end
+
 end
