46b8915ced6aef57f242d868a61ffeff27d292f8
vietleminh <leminhviet93@gmail.com>
2015-07-17 14:52:57 +0700

    refactor_user_activity

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @feed_activities = current_user.feed.recent.paginate page: params[:page],
+      					 per_page: Settings.length.page                                                          
+    end
   end
 end
